4bbcab55a8640636c545181cdad5f62606235fd3
Wirewords Academy <sarathm00@gmail.com>
2015-03-11 15:13:19 +0530

    "Done"

--- addRemote.py
+++ addRemote.py
@@ -1,5 +1,8 @@
 from subprocess import call as c
-com1c = "git remote add ssh3 git@github.com:SarathM1/dreadger"
+com1c = "git remote add origin https://github.com/SarathM1/dreadger.git"
 
 
 c(com1c.split())
+
+
+
